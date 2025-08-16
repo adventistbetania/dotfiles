9b12bdf6e59fbdc1c72fe6302fe90fc7a013b310
scriptogre <git@christiantanul.com>
2025-08-16 15:01:17 +0300

    Add static IP configuration and SSH authorized keys for admin user

--- flake.nix
+++ flake.nix
@@ -17,12 +17,24 @@
           # Networking
           networking.hostName = "betania";
           networking.networkmanager.enable = true;
+          networking.interfaces.eth0.ipv4.addresses = [
+            {
+              address = "192.168.0.128";  # Static IP address
+              prefixLength = 24;
+            }
+          ];
+
+          # SSH
+          services.openssh.enable = true;
 
           # User
           users.users.admin = {
             isNormalUser = true;
             extraGroups = [ "wheel" "networkmanager" "docker" ];
             password = "admin";
+            openssh.authorizedKeys.keys = [
+                "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDUJWuWXCZpiPQSwRZgOU6baccQZ14+lTJqUeMtfNE1jZcvtucF++3S7CTH7mHngFbI71/Io+ICqEZAkYBnu72CMwPXOwFyj5nerhQK5uX6KWGcLMXYwH4v43jWVdv/Xe/Dk3xshD3yevgeGzgQZxmWlko6hgr+0sGU7eJBFbfx8ILKTOLbXSVyBCx5xK37vaa8x7ZUB7oASj0hLH6YXs+BjPpwQuXCnNx8exMwMWajfaJ5gqaIyZLxyXJxgt8gbMTeQNN8fbavxiZozWwFbC50kXcHR8lKsGvXgqA5WlU55RdYoEzSTWflw6bsyEaFNbXBt2asAVDNBMPS1/aP8vdlKolU1Sqd/dMFYu1WLQ+Q705G//+iwEWeiZpg/m9+8CSU6OD0toRaUneC11CmDXTJjS89giIbofpz900+j2WOcSbAOEGHjCEl+qDl4bWXndF9itqgblQjFDysgJ3ZI4PAR4OCB8GyNY5UUoKg2HAB8H50gNdV0pHS2ysPh8c3Me/cGfbBYVUjzzsmxEd4VGzCP098ippMpgK/K/Q9TFtuaxEZZ3jOb0/GU10JjatgWYfULzXyXRkyptZDkGyGR+6I0YfcEmFy8hInf1oXS8keDHGAxznXn3EhNXH6xQuaJM4zIOE60CUUdBC3Iz1T7Kmidc9BNDGz/ggJmnD66RzTBw=="
+            ];
           };
           security.sudo.wheelNeedsPassword = false;
 
